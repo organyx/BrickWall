c00853935b4f5f3cc9a95b62eb0ed441166d31f5
organyx <aleksandr.beliun@gmail.com>
2015-09-05 20:48:00 +0100

    About Activity added

--- app/src/main/java/com/example/aleks/brickwall/MainActivity.java
+++ app/src/main/java/com/example/aleks/brickwall/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.aleks.brickwall;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,5 +35,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onBtnInfoClick(View view) {
+        Intent aboutIntent = new Intent(MainActivity.this, AboutActivity.class);
+        startActivity(aboutIntent);
     }
 }
